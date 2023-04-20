ef6a3f7816e2fd06d9f29fcc692d6d9b57fbe0df
CasperSidor <casper.sidor@gmail.com>
2023-04-20 12:54:23 +0200

    test1

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from './assets/Anders.jpg'
+import product2 from './assets/Eragon.jpg'
+import product3 from './assets/Krimi.jpg'
+import product4 from './assets/migfordig.jpg'
+import product5 from './assets/DragonbBall.jpg'
+import product6 from './assets/kaeftenlaekker.jpg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Tegneserie",
+        price: "4,95",
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Fantasy",
+        price: "29,95",
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Krimi",
+        price: "44,95",
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Romatisk Bog",
+        price: "75,00",
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Manga",
+        price: "15,00",
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Grammatik bog",
+        price: "24,95",
+        productImage: product6,
+    },
+];
